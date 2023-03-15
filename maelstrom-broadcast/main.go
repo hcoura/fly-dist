ba2a100a2b46df5078d9daab3c333beb794a9a53
Henrique Coura <coura.henrique@gmail.com>
2023-03-15 19:10:20 -0300

    Fix broadcast :)

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -4,55 +4,94 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"strconv"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type Gossip struct {
+type Message struct {
 	Type    string
+	MsgId   int `json:"msg_id,omitempty"`
 	Message int
 }
 
-var values []int
-var neighbors []string
+type Topology struct {
+	Type     string
+	Topology map[string][]string
+}
 
-func gossip(n *maelstrom.Node, message int) {
-	for _, neighbor := range neighbors {
-		go sendGossip(n, neighbor, message)
+type Gossip struct {
+	Src   string
+	Value int
+}
+
+type GossipsMsg struct {
+	Type    string
+	Gossips []Gossip
+}
+
+var values []int
+var toGossip []Gossip
+var neighbors []string
+var seen map[string]bool
+var mu sync.Mutex
+
+func gossip(n *maelstrom.Node) {
+	t := time.NewTicker(100 * time.Millisecond)
+	for range t.C {
+		var messages []Gossip
+		messages, toGossip = toGossip, []Gossip{}
+		for _, neighbor := range neighbors {
+			go sendGossips(n, neighbor, messages)
+		}
 	}
 }
 
-func sendGossip(n *maelstrom.Node, neighbor string, message int) {
-	m := map[string]any{
-		"type":    "gossip",
-		"message": message,
+func sendGossips(n *maelstrom.Node, neighbor string, gossips []Gossip) {
+	g := GossipsMsg{
+		Type:    "gossip",
+		Gossips: gossips,
 	}
 
 	// Just some silly way to handle network partition, if it timeouts in 500ms we try again
-	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if _, err := n.SyncRPC(ctx, neighbor, m); err != nil {
-		sendGossip(n, neighbor, message)
+	if _, err := n.SyncRPC(ctx, neighbor, g); err != nil {
+		sendGossips(n, neighbor, gossips)
 	}
+}
+
+func handleMessage(msg Message, id string) {
+	mu.Lock()
+	defer mu.Unlock()
+	if _, ok := seen[id]; ok {
+		return
+	}
+	values = append(values, msg.Message)
+	toGossip = append(toGossip, Gossip{Src: id, Value: msg.Message})
+	seen[id] = true
 }
 
 func main() {
 	n := maelstrom.NewNode()
+	mu = sync.Mutex{}
+	seen = map[string]bool{}
+	toGossip = []Gossip{}
+	neighbors = []string{}
+
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body map[string]any
+		var body Message
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		body["type"] = "broadcast_ok"
-		val := int(body["message"].(float64))
-		values = append(values, val)
-		go gossip(n, val)
-		delete(body, "message")
+		id := n.ID() + "_" + strconv.Itoa(body.MsgId)
+		handleMessage(body, id)
 
-		return n.Reply(msg, body)
+		return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -68,30 +107,39 @@ func main() {
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		res := map[string]string{"type": "topology_ok"}
-		return n.Reply(msg, res)
-	})
-
-	n.Handle("init", func(msg maelstrom.Message) error {
-		for _, node := range n.NodeIDs() {
-			if node != n.ID() {
-				neighbors = append(neighbors, node)
-			}
-		}
-
-		return nil
-	})
-
-	n.Handle("gossip", func(msg maelstrom.Message) error {
-		var body Gossip
+		var body Topology
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		values = append(values, body.Message)
+		neighbors = body.Topology[n.ID()]
+		res := map[string]string{"type": "topology_ok"}
+		return n.Reply(msg, res)
+	})
+
+	n.Handle("gossip", func(msg maelstrom.Message) error {
+		var gossip GossipsMsg
+		if err := json.Unmarshal(msg.Body, &gossip); err != nil {
+			return err
+		}
+
+		mu.Lock()
+		for _, a := range gossip.Gossips {
+			// It has already passed here, let's not propagate
+			if _, ok := seen[a.Src]; ok {
+				continue
+			}
+			toGossip = append(toGossip, a)
+			values = append(values, a.Value)
+			seen[a.Src] = true
+		}
+		mu.Unlock()
 
 		return nil
 	})
+
+	// keep gossiping as we accumulate messages
+	go gossip(n)
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
